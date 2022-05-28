0416c202f1cd5d51e115821e94c85095d3551376
ctsf1 <colin.tsf@gmail.com>
2022-05-28 11:54:59 +0100

    added zipper

--- snake/Contents/Resources/update_curl.sh
+++
@@ -1,4 +0,0 @@
-scr_path="/Applications/snake.app/contents/macos/snake"
-rm $scr_path
-curl -o $scr_path https://raw.githubusercontent.com/ctsf1/rust-snake/master/target/debug/snake
-chmod +x $scr_path
