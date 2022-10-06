c0f52eac6094aab2070082284a856d2072198d40
ctsf1 <79168898+ctsf1@users.noreply.github.com>
2022-10-06 12:41:55 +0100

    Delete setup_curl.sh

--- setup_curl.sh
+++
@@ -1,4 +0,0 @@
-appdir="/Applications/snake.app/contents/resources/update_curl.sh"
-touch ~/.zprofile; echo "\n"alias update-snake=\"$appdir\" >> ~/.zprofile
-curl -o $appdir https://raw.githubusercontent.com/ctsf1/rust-snake/master/update_curl.sh; chmod +x $appdir
-rm setup.sh
